4db71e9fa3d5b67659f2e241a080d937d3ffb070
rokaut990 <64099437+rokaut990@users.noreply.github.com>
2021-03-14 13:43:29 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    const usualBracketsStack = [];
+    str.split("").forEach((item) => {
+        bracketsConfig.forEach((config) => {
+            if (
+                item === config[0] &&
+                usualBracketsStack[usualBracketsStack.length - 1] !== config[1]
+            ) {
+                usualBracketsStack.push(config[0]);
+            } else if (
+                item === config[1] &&
+                config[0] === usualBracketsStack[usualBracketsStack.length - 1]
+            ) {
+                usualBracketsStack.pop();
+            }
+        });
+    });
+    return !usualBracketsStack.length;
+};
